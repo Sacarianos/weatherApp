984a8c01f60a0cf70c1adfe1d1b1c944b39d295c
Sacarianos <jaime.velez1@upr.edu>
2024-11-19 23:04:53 -0400

    Added a display weather function. next 5 day forecast, testing ,error, handleing, favorite cities and main menu

--- services.py
+++ services.py
@@ -12,9 +12,7 @@ def get_weather(zip_code, units='metric'):
 
     units = "imperial"
     
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={zip_code}&appid={api_key}&units={units}"
-    
-    print(url)
+    url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}&units={units}"
     
     response = requests.get(url)
 
@@ -24,3 +22,16 @@ def get_weather(zip_code, units='metric'):
     else:
         print(f"Error: {response.status_code}")
         return None
+
+def display_weather(weather_data):
+    if weather_data:
+        main = weather_data.get('main', {})
+        weather = weather_data.get('weather', [{}])[0]
+        sys = weather_data.get('sys', {})
+        temp = main.get('temp')
+        description = weather.get('description')
+        city = weather_data.get('name')
+       
+        print(f"This is the weather forecast for {city}")
+        print(f"Temperature: {temp}°")
+        print(f"Description: {description.capitalize()}")
--- weather.py
+++ weather.py
@@ -1,9 +1,13 @@
-from services import get_weather
+from services import get_weather, display_weather
 
 def main():
+    print("  ***Welcome to the Weather App***   \n\n")
+    
 
-    zip = input("Enter the zip code: ")
-    print(get_weather(zip, "imperial"))
+    zip = input("Enter the zip code for your location: ")
+    units = input("Choose temperature units (C for Celsius, F for Fahrenheit): ").lower()
+    units = 'metric' if units == 'c' else 'imperial'  # default to Celsius if invalid input
+    print(display_weather(get_weather(zip, "imperial")))
 
 
 if __name__ == "__main__":
