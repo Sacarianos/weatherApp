f662f098c8e873c1ae4833e4eeb30ca844ecd8b0
Sacarianos <jaime.velez1@upr.edu>
2024-11-20 13:24:00 -0400

    added menu and 5 day forecast

--- services.py
+++ services.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 # Load environment variables from .env file
 load_dotenv()
@@ -10,7 +11,6 @@ api_key = os.getenv('API_KEY')
 
 def get_weather(zip_code, units='metric'):
 
-    units = "imperial"
     
     url = f"http://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}&units={units}"
     
@@ -35,3 +35,42 @@ def display_weather(weather_data):
         print(f"This is the weather forecast for {city}")
         print(f"Temperature: {temp}°")
         print(f"Description: {description.capitalize()}")
+
+
+
+
+def display_forecast(forecast_data):
+    if forecast_data:
+        day = ""
+        for forecast in forecast_data.get('list', []):
+            dt_txt = forecast.get('dt_txt')
+            # Parse the input string into a datetime object
+            date_object = datetime.strptime(dt_txt, "%Y-%m-%d %H:%M:%S")
+            if day != date_object.strftime("%d"):
+                print(f"\nDate: {date_object.strftime("%Y-%m-%d")}")
+                day = date_object.strftime("%d")
+        
+            hour = date_object.strftime("%H")
+
+            temp = forecast.get('main', {}).get('temp')
+            description = forecast.get('weather', [{}])[0].get('description')
+            print(f"{hour}H - {temp}° - {description.capitalize()}")
+                                          
+
+
+def get_forecast(zip_code, units='metric'):
+    base_url = 'http://api.openweathermap.org/data/2.5/forecast'
+    
+    params = {
+        'zip': zip_code,
+        'appid': api_key,
+        'units': units  # 'metric' for Celsius, 'imperial' for Fahrenheit
+    }
+    
+    response = requests.get(base_url, params=params)
+    
+    if response.status_code == 200:
+        return response.json()  # Returns the forecast data as a Python dictionary
+    else:
+        print(f"Error: {response.status_code}")
+        return None
--- weather.py
+++ weather.py
@@ -1,13 +1,34 @@
-from services import get_weather, display_weather
+from services import get_weather, display_weather, display_forecast, get_forecast
 
 def main():
-    print("  ***Welcome to the Weather App***   \n\n")
-    
+    print("\n  ***Welcome to the Weather App***   \n\n")
 
-    zip = input("Enter the zip code for your location: ")
-    units = input("Choose temperature units (C for Celsius, F for Fahrenheit): ").lower()
-    units = 'metric' if units == 'c' else 'imperial'  # default to Celsius if invalid input
-    print(display_weather(get_weather(zip, "imperial")))
+    print("                MENU         ")
+    menu = 0 
+    while menu not in [1,2,3,4,5]:
+        menu = int(input("1. Check current weather \n2. Check 5 day forecast\n3. Favorite cities\n4. add Favorite citiy \n5. Delete favorite city\n"))
+        if menu not in [1,2,3,4,5]:
+            print("\nWrong input\n")
+        
+        match menu:
+            case 1:
+                zip = input("Enter the zip code for your location: ")
+                units = input("Choose temperature units (C for Celsius, F for Fahrenheit): ").lower()
+                units = 'metric' if units == 'c' else 'imperial'  # default to Celsius if invalid input
+                print(display_weather(get_weather(zip, units)))
+            case 2:
+                zip = input("Enter the zip code for your location: ")
+                units = input("Choose temperature units (C for Celsius, F for Fahrenheit): ").lower()
+                units = 'metric' if units == 'c' else 'imperial'  # default to Celsius if invalid input
+                print(display_forecast(get_forecast(zip, units)))
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+    
+    
 
 
 if __name__ == "__main__":
